7be8c665bb9fb5326e1f5091996692976cb2398f
horichii <virgilianvidal@gmail.com>
2026-03-19 09:52:51 +0800

    feat: added autogenerateship method and placement checker method

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,3 +1,5 @@
+import { Ship } from "./ship.js";
+
 export class Gameboard {
     constructor() {
         this.board = [];
@@ -28,14 +30,18 @@ export class Gameboard {
 
         if (this.board[row][col] === null) { //miss
             this.board[row][col] = "miss";
-        } else if (this.board[row][col] === "miss") { //already marked miss
-            return;
+            return "miss";
+        } else if (this.board[row][col] === "miss" || this.board[row][col] === "hit") { 
+            return "Already attacked";
         } else { //ship hit
             this.board[row][col].hit();
+            this.board[row][col] = "hit"; 
+            return "hit";
         }
     }
 
     areAllShipsSunk() {
+        // console.log("array ships: ", this.ships);
         for(let i = 0; i < this.ships.length; i++) {
             this.ships[i].isSunk();
             if (this.ships[i].isSunk() === false) {
@@ -43,5 +49,64 @@ export class Gameboard {
             }
         }
         return true;
+    }
+
+    autoGenerateShips() {
+        this.board = [];
+        this.ships = [];
+        for (let i = 0; i < 10; i++) {
+            this.board.push(new Array(10).fill(null));
+        }
+
+        const shipLengths = [5, 4, 3, 2, 1];
+
+        for (let i = 0; i < shipLengths.length; i++){
+            let length = shipLengths[i];
+            let isPlaced = false;
+
+            while (isPlaced === false) {
+                const row = Math.floor(Math.random() * 10);
+                const col = Math.floor(Math.random() * 10);
+
+                let direction;
+                if (Math.random() > 0.5) {
+                    direction = "horizontal";
+                } else {
+                    direction = "vertical";
+                }
+
+                if (this.isValidPlace(length, row, col, direction)) {
+                    this.placeShip(new Ship(length), row, col, direction);
+                    isPlaced = true;
+                }
+            }
+        }
+    }
+
+    isValidPlace(length, row, col, direction) {
+        if (direction === "horizontal") {
+            
+            if (col + length > 10) return false; 
+            
+            
+            for (let i = 0; i < length; i++) {
+                if (this.board[row][col + i] !== null) {
+                    return false;
+                }
+            }
+        } else if (direction === "vertical") {
+            
+            if (row + length > 10) {
+                return false;
+            }
+            
+            
+            for (let i = 0; i < length; i++) {
+                if (this.board[row + i][col] !== null) {
+                    return false; 
+                }
+            }
+        }
+        return true; 
     }
 }
